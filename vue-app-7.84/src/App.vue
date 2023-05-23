9f4f41f074575d884157e0e42ada528c8cf219f1
Hal Jordan <hjordan836@gmail.com>
2023-05-23 20:04:08 +0000

    Push code. Setup NPM.

---
+++ vue-app-7.84/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <header>
+            <h1>My Friends</h1>
+        </header>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manual@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julia@localhost.com',
+                },
+            ],
+        };
+    },
+};
+</script>
