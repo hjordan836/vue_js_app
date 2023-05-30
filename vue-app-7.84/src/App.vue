6c91c092fab5ccf44e0c84e1cbdbacaf8cfd9803
Hal Jordan <hjordan836@gmail.com>
2023-05-30 14:25:51 +0000

    Pushing code

--- vue-app-7.84/src/App.vue
+++ vue-app-7.84/src/App.vue
@@ -1,34 +1,63 @@
 <template>
-    <section>
-        <header>
-            <h1>My Friends</h1>
-        </header>
-        <ul>
-            <friend-contact></friend-contact>
-            <friend-contact></friend-contact>
-        </ul>
-    </section>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <new-friend @add-contact="addContact"></new-friend>
+    <ul>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+      ></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            friends: [
-                {
-                    id: 'manuel',
-                    name: 'Manuel Lorenz',
-                    phone: '01234 5678 991',
-                    email: 'manual@localhost.com',
-                },
-                {
-                    id: 'julie',
-                    name: 'Julie Jones',
-                    phone: '09876 543 221',
-                    email: 'julia@localhost.com',
-                },
-            ],
-        };
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manual@localhost.com",
+          isFavorite: true,
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julia@localhost.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(contactData) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name: contactData.name,
+        phone: contactData.phone,
+        email: contactData.email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriend);
+    },
+  },
 };
 </script>
--- vue-app-7.84/src/components/FriendContact.vue
+++ vue-app-7.84/src/components/FriendContact.vue
@@ -1,99 +1,151 @@
 <template>
-    <li>
-        <h2>{{ friend.name }}</h2>
-        <button @click="toggleDetails">
-            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
-        </button>
-        <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ friend.phone }}</li>
-            <li><strong>Email:</strong> {{ friend.email }}</li>
-        </ul>
-    </li>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            detailsAreVisible: false,
-            friend: {
-                id: 'manuel',
-                name: 'Manuel Lorenz',
-                phone: '01234 5678 991',
-                email: 'manual@localhost.com',
-            },
-        };
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    id: {
+      type: String,
+      required: true,
     },
-    methods: {
-        toggleDetails() {
-            this.detailsAreVisible = !this.detailsAreVisible;
-        },
+    name: {
+      type: String,
+      required: true,
     },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value) {
+      //     return value === "1" || value === "0";
+    },
+  },
+  emits: ["toggle-favorite"],
+  //   emits: {
+  //     "toggle-favorite": function (id) {
+  //       if (id) {
+  //         return true;
+  //       } else {
+  //         console.log("id is not defined");
+  //         return false;
+  //       }
+  //     },
+  //   },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
 };
 </script>
 
 <style>
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 html {
-    font-family: 'Jost', sans-serif;
+  font-family: "Jost", sans-serif;
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 
 header {
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    margin: 3rem auto;
-    border-radius: 10px;
-    padding: 1rem;
-    background-color: #58004d;
-    color: white;
-    text-align: center;
-    width: 90%;
-    max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 
 #app ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
+#app form,
 #app li {
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    margin: 1rem auto;
-    border-radius: 10px;
-    padding: 1rem;
-    text-align: center;
-    width: 90%;
-    max-width: 40rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
 }
 
 #app h2 {
-    font-size: 2rem;
-    border-bottom: 4px solid #ccc;
-    color: #58004d;
-    margin: 0 0 1rem 0;
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
 }
 
 #app button {
-    font: inherit;
-    cursor: pointer;
-    border: 1px solid #ff0077;
-    background-color: #ff0077;
-    color: white;
-    padding: 0.05rem 1rem;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
 
 #app button:hover,
 #app button:active {
-    background-color: #ec3169;
-    border-color: #ec3169;
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
