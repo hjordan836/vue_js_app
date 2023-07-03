d7002239c63efd48820c41fad62620c3431766d1
hjordan836 <hjordan836@gmail.com>
2023-07-03 19:51:55 +0000

    Code for unit 15 - VueX/Managing State

---
+++ code_section-15.212/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:vue/vue3-essential",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
